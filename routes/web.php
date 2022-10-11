8465c3bc287de218385768f29dc3e64490bc95e8
AaEzha <aaezha@gmail.com>
2022-10-11 21:16:25 +0700

    fix: sidebar

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-  return view('welcome');
+  return view('welcome', [
+    'type_menu' => null
+  ]);
 })->middleware('auth');
 
 Route::get('/logout', function () {
