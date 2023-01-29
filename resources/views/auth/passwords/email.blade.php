514b703f3c1453bac9e1f947c4de4e3bcd7c5cc2
nengwiwik <wiwiklistiyarini545@gmail.com>
2023-01-29 10:50:06 +0700

    fix: layout for reset password 🦆

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,90 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="shortcut icon" href="{{ asset('image/icon.png') }}" width="25px" height="25px">
+
+  <title>Reset Password | {{ env('APP_NAME') }}</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="{{ asset('template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css') }}">
+  <link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="{{ asset('template/css/sb-admin-2.min.css') }}" rel="stylesheet">
+</head>
+<img src="" alt="">
+
+<body class=""
+  style="background:url('{{ asset('image/undira.jpeg') }}') no-repeat center center fixed; -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;">
+  <div class="container">
+    <!-- Outer Row -->
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
+      <div class="col-md-6">
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="p-5">
+                  <div class="text-center">
+                    <img src="{{ asset('image/logo_baru.png') }}" alt="UNDIRA">
+                    <h1 class="h4 text-gray-900 mb-4">{{ __('Reset Password') }}</h1>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
+                  </div>
+                  <form class="user" method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                    <div class="form-group">
+                      <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror"
+                        id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Email" name="email"
+                        value="{{ old('email') }}" autofocus autocomplete="off">
+                      @error('email')
+                        <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                        </span>
+                      @enderror
+                    </div>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                      <i class="fas fa-sign-out "></i> {{ __('Send Password Reset Link') }}
+                    </button>
+                  </form>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                  <div class="text-center mt-3">
+                    <a class="small" href="{{ route('login') }}">
+                      Remember your account?
+                    </a>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-@endsection
+  </div>
+  </div>
+  <!-- Bootstrap core JavaScript-->
+  <script src="{{ asset('template/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('template/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="{{ asset('template/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+  <!-- Custom scripts for all pages-->
+  <script src="{{ asset('template/js/sb-admin-2.min.js') }}"></script>
+</body>
+
+</html>
