8c7fcc3df8a80a5e2fc8f8d8c9283caad789e21c
nengwiwik <wiwiklistiyarini545@gmail.com>
2023-09-05 02:51:14 +0700

    fix: hilangkan total harga pada dashboard Dosen 🔥

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -147,7 +147,7 @@
         </div>
       @endrole
 
-      @hasanyrole('Super Admin|Penerbit|Admin Prodi')
+      @hasanyrole('Super Admin|Penerbit')
         {{-- Nominal Pengadaan Aktif --}}
         <div class="col-xl-3 col-md-6 mb-4">
           <div class="card border-left-success shadow h-100 py-2">
