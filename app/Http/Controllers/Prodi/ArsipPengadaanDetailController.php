b3bfdb591f7e87f3ffaff1708c26e361243d008e
nengwiwik <wiwiklistiyarini545@gmail.com>
2023-09-06 04:10:31 +0700

    fix 🔥

--- app/Http/Controllers/Prodi/ArsipPengadaanDetailController.php
+++ app/Http/Controllers/Prodi/ArsipPengadaanDetailController.php
@@ -25,34 +25,27 @@ class ArsipPengadaanDetailController extends GroceryCrudController
             $table . '.procurement_id = ?' => $procurement->getKey(),
             $table . '.deleted_at is null',
             $table . '.major_id = ?' => Auth::user()->major_id,
-            // $table . '.is_chosen = ?' => 1,
+            $table . '.is_chosen = ?' => 1,
         ]);
 
         $crud->unsetOperations();
-        $crud->columns(['is_chosen', 'title', 'published_year', 'isbn', 'author_name', 'suplemen', 'cover']);
-        $crud->readFields(['title'. 'cover', 'is_chosen', 'major_id', 'published_year', 'isbn', 'author_name', 'price', 'suplemen']);
-        $crud->setRelation('major_id', 'majors', 'name');
-        $crud->defaultOrdering('is_chosen', 'desc');
+        $crud->columns(['title', 'published_year', 'isbn', 'author_name', 'suplemen']);
+        $crud->readFields(['title', 'summary', 'is_chosen', 'published_year', 'isbn', 'author_name', 'price', 'suplemen']);
         $crud->fieldType('price', 'numeric');
-        $crud->fieldType('eksemplar', 'numeric');
-        $crud->fieldType('is_chosen', 'checkbox_boolean');
-        $crud->setRule('eksemplar', 'min', '0');
         $crud->displayAs([
             'major_id' => 'Jurusan',
             'isbn' => 'ISBN',
             'published_year' => 'Tahun Terbit',
             'author_name' => 'Nama Penulis',
-            'suplemen' => 'Suplemen',
-            'is_chosen' => 'Terpilih',
-            'price' => 'Harga',
             'title' => 'Judul Buku',
+            'suplemen' => 'Suplemen',
+            'price' => 'Harga',
+        ]);
+        $crud->fieldType('is_chosen', 'dropdown_search', [
+            1 => 'Ya',
+            0 => 'Tidak',
         ]);
         $crud->setTexteditor(['summary']);
-        $crud->setFieldUpload('cover', 'storage', asset('storage'));
-        $crud->callbackColumn('cover', function ($value, $row) {
-            $data = ProcurementBook::find($row->id);
-            return "<img src='" . $data->cover . "' height='150'>";
-        });
         $crud->callbackReadField('price', function ($value, $row) {
             return "IDR " . number_format($value, 0, ',', '.');
         });
